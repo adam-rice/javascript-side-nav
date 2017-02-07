7b430aeda1204e2a29c5c3cff1fb3e2039d79f4f
Adam Rice <adamcoltonrice@gmail.com>
2017-02-07 11:34:18 -0700

    Add onTransitionEnd method

--- side-nav.js
+++ side-nav.js
@@ -13,6 +13,11 @@ class SideNav {
     this.hideSideNav = this.hideSideNav.bind(this);
     this.blockClicks = this.blockClicks.bind(this);
     this.onTouchStart = this.onTouchStart.bind(this);
+    this.onTouchMove = this.onTouchMove.bind(this);
+    this.onTouchEnd = this.onTouchEnd.bind(this);
+
+    this.startX = 0;
+    this.currentX = 0;
 
 
     this.addEventListeners();
@@ -23,23 +28,50 @@ class SideNav {
     this.hideButtonEl.addEventListener('click', this.hideSideNav);
     this.sideNavEl.addEventListener('click', this.hideSideNav);
     this.sideNavContainerEl.addEventListener('click', this.blockClicks);
+
     this.sideNavContainerEl.addEventListener('touchstart', this.onTouchStart);
+    this.sideNavContainerEl.addEventListener('touchmove', this.onTouchMove);
+    this.sideNavContainerEl.addEventListener('touchend', this.onTouchEnd);
   }
 
   onTouchStart(e) {
-    
+    e.preventDefault();
+    this.startX = e.touches[0].pageX;
+    this.currentX = this.startX;
+  }
+
+  onTouchMove(e) {
+    this.currentX = e.touches[0].pageX;
+
+    const translateX = this.currentX - this.startX;
+    this.sideNavEl.style.transform = `${translateX}`;
+  }
+
+  onTouchEnd(e) {
+    // this.currentX = e.touches[0].pageX;
   }
 
   blockClicks(e) {
     e.stopPropogation();
   }
 
+  onTransitionEnd (e) {
+  this.sideNavEl.classList.remove('side-nav--animatable');
+  this.sideNavEl.removeEventListener('transitionend', this.onTransitionEnd);
+}
+
   showSideNav() {
-    this.sideNavEl.classList.add('.side-nav--visible');
+    this.sideNavEl.classList.add('side-nav--animatable');
+    this.sideNavEl.classList.add('side-nav--visible');
+    this.detabinator.inert = false;
+    this.sideNavEl.addEventListener('transitionend', this.onTransitionEnd);
   }
 
   hideSideNav() {
-    this.sideNavEl.classList.remove('.side-nav--visible');
+    this.sideNavEl.classList.add('side-nav--animatable');
+    this.sideNavEl.classList.remove('side-nav--visible');
+    this.detabinator.inert = true;
+    this.sideNavEl.addEventListener('transitionend', this.onTransitionEnd);
   }
 }
 

09f0b9c030cf2b84532c0890889beec02da8eacc
Adam Rice <adamcoltonrice@gmail.com>
2017-02-07 09:34:19 -0700

    wip side-nav.js

--- side-nav.js
+++ side-nav.js
@@ -1,1 +1,20 @@
 /*jshint esversion: 6 */
+
+'use strict';
+
+class SideNav {
+  constructor () {
+    this.sideNavEl = document.querySelector('.js-menu');
+    this.toggleButtonEl = document.querySelector('.js-side-nav');
+
+    this.showSideNav = this.showSideNav.bind(this);
+
+    this.addEventListeners();
+  }
+
+  addEventListeners() {
+    this.toggleButtonEl('click', this.showSideNav);
+  }
+}
+
+new SideNav();
